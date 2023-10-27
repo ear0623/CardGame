4917163bf4c07311b7ef45a64695f8f34778d9ff
NahYoungLyroal <ear062311@gamil.com>
2023-10-27 13:08:38 +0900

    클래스로 정렬

--- main.cpp
+++ main.cpp
@@ -1,28 +1,74 @@
 #include <iostream>
 #include <ctime>
 
-using namespace std;
+#define MaxNumber 52
 
+using namespace std;
+class NumberExtract
+{
+public:
+	NumberExtract();
+	~NumberExtract();
+
+	void start();
+
+private:
+
+	int Array[52];
+};
+
+NumberExtract::NumberExtract()
+{
+}
+
+NumberExtract::~NumberExtract()
+{
+}
+void NumberExtract::start()
+{
+	cout << "임의 숫자 추출" << endl;
+	for (int i = 0; i < MaxNumber; i++)
+	{
+		Array[i] = i + 1;
+	}
+
+
+	for (int i = 0; i < 1000; i++)
+	{
+		int First = rand() % 52;
+		int Second = rand() % 52;
+		//
+		int Temp = Array[First];
+		Array[First] = Array[Second];
+		Array[Second] = Temp;
+		//
+	}
+	for (int i = 0; i < 4; ++i)
+	{
+		cout << Array[i] << " ";
+	}
+}
 int main()
 {
 	//1~52 중 랜덤 숫자
-	srand(time(nullptr));
-	cout << "임의 숫자 추출" << endl;
-	for (int i = 0; i <= 4; i++)
-	{
-		cout << (rand() % 52)<<" ";
-	}
+	srand((unsigned int)time(nullptr));
+	int Array[52];//0~51
+
 	
 
+	cout << endl;
+	cout << endl;
+	cout << endl;
+	cout << endl;
 
-	cout << endl;
-	cout << endl;
-	cout << endl;
+
 	//금일 로또 추천 번호
 	cout << "이번주 로또 추천번호" << endl;
-	for  (int i = 1; i <= 7; i++)
+	for (int i = 1; i <= 7; i++)
 	{
-		cout << rand() % 52<<" ";
+		Array[i] = (rand() % 52);
+		cout << Array[i] << " ";
 	}
-	cout<< endl;
+	cout << endl;
 }
+	
