131dffff04db1823602f287f43e7d02d2571f677
NahYoungLyroal <ear062311@gamil.com>
2023-10-27 17:48:12 +0900

    랜덤숫자 추출

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ private:
 
 NumberExtract::NumberExtract()
 {
+
 }
 
 NumberExtract::~NumberExtract()
@@ -26,6 +27,7 @@ NumberExtract::~NumberExtract()
 }
 void NumberExtract::start()
 {
+	srand((unsigned int)time(nullptr));
 	cout << "임의 숫자 추출" << endl;
 	for (int i = 0; i < MaxNumber; i++)
 	{
@@ -45,30 +47,84 @@ void NumberExtract::start()
 	}
 	for (int i = 0; i < 4; ++i)
 	{
-		cout << Array[i] << " ";
+		
+		int CardType = (Array[i]) / 13;
+		if (CardType == 0)
+		{
+			cout << "spade" << endl;
+		}
+		else if (CardType == 1)
+		{
+			cout << "CLover" << endl;
+		}
+		else if (CardType == 2)
+		{
+			cout << "HEart" << endl;
+		}
+		else
+		{
+			cout << "Diamond" << endl;
+		}
+
+		int CardNumber = Array[0] % 13;
+
+		if (CardNumber == 10)
+		{
+			cout << "J" << endl;
+		}
+		else if (CardNumber == 12)
+		{
+			cout << "K" << endl;
+		}
+		else if (CardNumber == 13)
+		{
+			cout << "Q" << endl;
+		}
+		else
+		{
+			cout << Array[i] << endl;
+		}
+
+		int PlayerScore = 0;
+		int ComputerScore = 0;
+
+		if (i<2)
+		{
+			if (CardNumber > 10)
+			{
+				ComputerScore += 10;
+			}
+			else
+			{
+				ComputerScore += CardNumber;
+			}
+		}
+		else
+		{
+			if (CardNumber>10)
+			{
+				PlayerScore += 10;
+			}
+			else
+			{
+				PlayerScore += CardNumber;
+			}
+
+		}
 	}
+
+	
 }
 int main()
 {
 	//1~52 중 랜덤 숫자
-	srand((unsigned int)time(nullptr));
-	int Array[52];//0~51
+	
+	
+	NumberExtract numbverextract;
+	numbverextract.start();
+
+	cout << endl;
 
 	
-
-	cout << endl;
-	cout << endl;
-	cout << endl;
-	cout << endl;
-
-
-	//금일 로또 추천 번호
-	cout << "이번주 로또 추천번호" << endl;
-	for (int i = 1; i <= 7; i++)
-	{
-		Array[i] = (rand() % 52);
-		cout << Array[i] << " ";
-	}
-	cout << endl;
 }
 	
